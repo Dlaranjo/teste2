ffc6cda84f838aabe528935356cc5a394c7c2b5b
Dlaranjo <146372233+Dlaranjo@users.noreply.github.com>
2023-09-28 14:23:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var openBlipChat;
+let tooltipInstances;
+
+window.onload = function () {
+		
+	const appkey = "dGVzdGVncHQxODphZjA5ZDYyNy05MjcxLTRhYTgtYjM3Ny05NDRhNTM2NzQ5YTk=";
+
+	var client = new BlipChat()
+	.withAppKey(appkey)
+	.withEventHandler(BlipChat.LOAD_EVENT, function(){
+		var iframe = document.getElementById("blip-chat-iframe");
+		iframe.contentWindow.postMessage({ code: "ShowCloseButton", showCloseButton: true }, iframe.src);	
+	});
+		
+	client.build();
+
+	openBlipChat = function() {
+		client.widget._openChat();
+	}
+	
+	M.FloatingActionButton.init(document.querySelectorAll('.fixed-action-btn'));
+  tooltipInstances = M.Tooltip.init(document.querySelectorAll('.tooltipped'));
+  
+  window.addEventListener('scroll', () => {
+    for(let i = 0; i < tooltipInstances.length; ++i) {
+      tooltipInstances[i]._positionTooltip();
+    }
+  });
+
+}
